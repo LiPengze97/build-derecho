357489854b7f3150518a3fe586a8f1d03e007711
Pan Lichen <myles.l.pan@gmail.com>
2020-09-25 08:22:36 -0700

    add cfg and script useful when testing derecho

---
+++ temp_derecho_build.sh
@@ -0,0 +1,6 @@
+INSTALL_PREFIX="/usr/local"
+
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ..
+make -j `lscpu | grep "^CPU(" | awk '{print $2}'`
+sudo make install
