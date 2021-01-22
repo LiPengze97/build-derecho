006feacfbce9726158d249f2ecac2fb9c3203b0e
Panlichen <myles.l.pan@gmail.com>
2021-01-22 12:23:30 +0800

    update

--- clean-log.sh
+++ clean-log.sh
@@ -1,9 +1,19 @@
-for path in `find /users/weijia/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.plog"`
+# for path in `find /users/weijia/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.plog"`
+# do
+#     rm -rf $path
+# done
+
+# for path in `find /users/weijia/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.log"`
+# do
+#     rm -rf $path
+# done
+
+for path in `find /home/mlcp/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.plog"`
 do
     rm -rf $path
 done
 
-for path in `find /users/weijia/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.log"`
+for path in `find /home/mlcp/cascade/build/src/service/data_path_logic/cnn_classifier_cfg -regex ".*\.log"`
 do
     rm -rf $path
 done
